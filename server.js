c70483c43cb74a9e9c51d71362f66f50cde3679d
Knight2 <8440420+knight2@users.noreply.github.com>
2018-12-12 12:09:56 -0500

    handle get /api/timestamp/:date?

--- server.js
+++ server.js
@@ -24,9 +24,36 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:date?", function(req, res){
+  
+  var date = null;
+  //parsing date string
+  if (req.params.date !== undefined){
+
+    var unixTimestamp = parseInt(req.params.date*1); //checking if date submitted is unix time stamp
+    if (isNaN(unixTimestamp)) { //is not a unix time stamp
+      submitted_date = new Date(req.params.date);
+    }
+    else { //it is a timestamp
+      submitted_date = new Date(unixTimestamp);
+    }
+  }
+  else //string is empty, give current time
+  {
+    submitted_date = new Date(Date.now());
+  }
+
+  if (date == "Invalid Date"){
+    res.json("invalid date");
+  } else{
+    res.json({"unix": submitted_date.getTime(), "utc": submitted_date.toUTCString()});
+  }
+
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
